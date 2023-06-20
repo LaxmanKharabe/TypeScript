403115a50f0d189d5b0edefa48facb4d99d1b319
Laxman <66318636+LaxmanKharabe@users.noreply.github.com>
2023-06-21 00:12:44 +0530

    Add files via upload

---
+++ PrimitiveDatatypes/NmberDataType/script.js
@@ -0,0 +1,37 @@
+// SIgned Integer
+document.write("<h3>1) SIgned Integer</h3>");
+var signedInt1 = +10;
+var signedInt2 = -20;
+document.write("1) Signed Integer variable 1: " + signedInt1 + " 2) Signed Integer variable 2: " + signedInt2);
+// Un SIgned Integer
+document.write("<h3>2) Un SIgned Integer</h3>");
+var unSignedInt = 10;
+document.write("Un Signed Integer Variable: " + unSignedInt);
+// floatNum Integer
+document.write("<h3>3) Float Integer</h3>");
+var floatNum = 10.12;
+document.write("Float Integer Variable: " + floatNum);
+// Double Integer
+document.write("<h3>4) Double Integer</h3>");
+var doubleNum = 100.345;
+document.write("Double Integer Variable: " + doubleNum);
+// Decimal Integer
+document.write("<h3>5) Double Integer:</h3>");
+var decimalNum = 102130322.1324256782345123456;
+document.write("Double Integer Variable: " + decimalNum);
+// Exponent Integer
+document.write("<h3>6) Exponent Integer:</h3>");
+var exponentNum = 2e3;
+document.write("Exponent Integer Variable: " + exponentNum);
+// Binary Integer
+document.write("<h3>7) Binary Integer:</h3>");
+var binaryNum = 10;
+document.write("Binary Integer Variable: " + binaryNum);
+// Octal Integer
+document.write("<h3>7) Octal Integer:</h3>");
+var octalNum = 484;
+document.write("Octal Integer Variable: " + octalNum);
+// Hexadecimal Integer
+document.write("<h3>7) Hexadecimal Integer:</h3>");
+var hexadecimalNum = 0xf00d;
+document.write("Octal Integer Variable: " + hexadecimalNum);
---
+++ VarLetConst/app.js
@@ -0,0 +1,22 @@
+function hoistingExample() {
+    z = 10;
+    console.log("Hoisted variable z value: " + z); // 10
+}
+var z; //Hoisting 
+function shadowingExample() {
+    var x = 10;
+    if (true) {
+        var x_1 = 20; // Shadowing the outer 'x'
+        console.log("Shadowing variable x value: " + x_1); // 20
+    }
+    console.log("Out side of a block scope vale of x: " + x); // 10
+}
+hoistingExample();
+shadowingExample();
+function f1() {
+    //   const a;  we can not declare "const" varible we have to initialize the constant value to it
+    var score1 = "Const variable";
+    // const score1 : string = "Laxman";  cant be re-declare "const" variable
+    console.log(score1);
+}
+f1();
