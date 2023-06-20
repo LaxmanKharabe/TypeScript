513b7b790250541e4a01172d3e7b2b035bf7690c
Laxman <66318636+LaxmanKharabe@users.noreply.github.com>
2023-06-21 00:15:16 +0530

    Add files via upload

---
+++ PrimitiveDatatypes/NmberDataType/StringDataType/script.ts
@@ -0,0 +1,45 @@
+// SIgned Integer
+document.write("<h3>1) SIgned Integer</h3>");
+var signedInt1: number = +10;
+var signedInt2: number = -20;
+document.write("1) Signed Integer variable 1: " + signedInt1 + " 2) Signed Integer variable 2: " + signedInt2);
+
+// Un SIgned Integer
+document.write("<h3>2) Un SIgned Integer</h3>");
+var unSignedInt: number = 10;
+document.write("Un Signed Integer Variable: " + unSignedInt);
+
+// floatNum Integer
+document.write("<h3>3) Float Integer</h3>");
+var floatNum: number = 10.12;
+document.write("Float Integer Variable: " + floatNum);
+
+// Double Integer
+document.write("<h3>4) Double Integer</h3>");
+var doubleNum: number = 100.345;
+document.write("Double Integer Variable: " + doubleNum);
+
+// Decimal Integer
+document.write("<h3>5) Double Integer:</h3>");
+var decimalNum: number = 102130322.1324256782345123456;
+document.write("Double Integer Variable: " + decimalNum);
+
+// Exponent Integer
+document.write("<h3>6) Exponent Integer:</h3>");
+var exponentNum: number = 2e3;
+document.write("Exponent Integer Variable: " + exponentNum);
+
+// Binary Integer
+document.write("<h3>7) Binary Integer:</h3>");
+var binaryNum: number = 0b1010;
+document.write("Binary Integer Variable: " + binaryNum);
+
+// Octal Integer
+document.write("<h3>7) Octal Integer:</h3>");
+var octalNum: number = 0o744;
+document.write("Octal Integer Variable: " + octalNum);
+
+// Hexadecimal Integer
+document.write("<h3>7) Hexadecimal Integer:</h3>");
+var hexadecimalNum: number = 0xf00d;
+document.write("Octal Integer Variable: " + hexadecimalNum);
